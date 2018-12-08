58047e411c4c3d1e3ba6002b953d73047b82365e
Wei <weinini007@gmail.com>
2018-12-08 22:42:32 +1100

    chore: updated gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,52 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
+/*
 
+Gulp runs in a folder structure like this:
+
+- app/              (all files for development)
+  - index.html
+  - css/
+  - img/
+  - js/
+  - scss/
+- dist/             (files ready for distribution)
+- gulpfile.js
+- node_modules/
+- package.json
+- .gitignore
+
+*/
+
+// Require dependencies from node_modules
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+// Task to compile sass to css
 gulp.task('sass', function(){
-  return gulp.src('app/scss/styles.scss')
+  // Check all scss files in directory using globe pattern
+  return gulp.src('app/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    // Inyect css changes after processing scss file/s
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// Task to watch for changes in sass, js and html files after browserSync has finished executing
+gulp.task('watch', ['browserSync'], function() {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+// Task to start server for browserSync
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
 })
 
 // gulp.task('default', defaultTask);
